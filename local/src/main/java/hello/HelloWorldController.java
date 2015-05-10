b7cbd3ae609bcfcab5d6096374c2d1416c963730
Brian Oxley <boxley@thoughtworks.com>
2015-05-10 15:23:21 -0500

    More swagger api.

--- local/src/main/java/hello/HelloWorldController.java
+++ local/src/main/java/hello/HelloWorldController.java
@@ -1,6 +1,12 @@
 package hello;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +21,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * @author <a href="mailto:boxley@thoughtworks.com">Brian Oxley</a>
  * @todo Needs documentation
  */
+@Api(value = "hello-world-controller", description = "Hello, World!")
 @RestController
 public class HelloWorldController {
     private final HystrixRemoteHello remote;
@@ -24,9 +31,18 @@ public class HelloWorldController {
         this.remote = remote;
     }
 
+    @ApiOperation("Say hello")
+    @ApiResponses({@ApiResponse(code = 203,
+            message = "Remote services are unavailable"),
+            @ApiResponse(code = 400,
+                    message = "Recheck your X-Correlation-ID")})
     @RequestMapping(value = "/hello/{name}", method = GET)
-    public Greeting hello(@PathVariable final String name,
-            final HttpServletResponse response) {
+    public Greeting hello(
+            @ApiParam("What is your name?") @PathVariable final String name,
+            @SuppressWarnings("UnusedParameters")
+            @ApiParam("I want to track you")
+            @RequestHeader(value = "X-Correlation-ID", required = true)
+            final String correlationID, final HttpServletResponse response) {
         return remote.greet(In.builder().name(name).build(), response);
     }
 }
