fc8839f0e84c9366b0a282ebad8318da3c57361b
Brian Oxley <boxley@thoughtworks.com>
2015-05-10 14:21:31 -0500

    Better remote-hello health.

--- local/src/main/java/hello/health/RemoteHello.java
+++ local/src/main/java/hello/health/RemoteHello.java
@@ -2,6 +2,7 @@ package hello.health;
 
 import hello.FeignRemoteHello;
 import hello.In;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health.Builder;
 import org.springframework.stereotype.Component;
@@ -17,10 +18,14 @@ import javax.inject.Inject;
 @Component
 public class RemoteHello
         extends AbstractHealthIndicator {
+    private final String service;
     private final FeignRemoteHello feign;
 
     @Inject
-    public RemoteHello(final FeignRemoteHello feign) {
+    public RemoteHello(
+            @Value("${spring.application.name}") final String service,
+            final FeignRemoteHello feign) {
+        this.service = service;
         this.feign = feign;
     }
 
@@ -28,14 +33,12 @@ public class RemoteHello
     protected void doHealthCheck(final Builder builder)
             throws Exception {
         try {
-            feign.greet(In.builder().name("self").build());
+            feign.greet(In.builder().name(service).build());
             builder.up().
-                    withDetail("remote-hello",
-                            "http://remote-hello/greet/self");
+                    withDetail("endpoint", "http://remote-hello/greet");
         } catch (final Exception e) {
             builder.outOfService().
-                    withDetail("remote-hello",
-                            "http://remote-hello/greet/self").
+                    withDetail("endpoint", "http://remote-hello/greet").
                     withDetail("exception", e.getMessage());
         }
     }
