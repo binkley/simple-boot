195f802278c856b99d058a46ddd4feddc0b26f9c
Brian Oxley <boxley@thoughtworks.com>
2015-06-01 19:03:48 -0400

    WIP.

--- common/src/main/java/hello/RibbonLogging.java
+++ common/src/main/java/hello/RibbonLogging.java
@@ -1,12 +1,17 @@
 package hello;
 
+import com.netflix.client.RetryHandler;
+import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerStats;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.netflix.ribbon.RibbonClientConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClientSpecification;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -27,8 +32,35 @@ public class RibbonLogging
     }
 
     @Configuration
+    @RibbonClients
     @AutoConfigureAfter(RibbonClientConfiguration.class)
     public static class RibbonLoggingConfiguration<T extends Server> {
+        public RibbonLoadBalancerContext loggingRibbonContext(final ILoadBalancer balancer, final
+                IClientConfig config) {
+            return new RibbonLoadBalancerContext(balancer, config) {
+                @Override
+                public void noteRequestCompletion(final ServerStats stats,
+                        final Object response, final Throwable e,
+                        final long responseTime) {
+                    System.out.println(
+                            "RibbonLoggingConfiguration.noteRequestCompletion");
+                    super.noteRequestCompletion(stats, response, e,
+                            responseTime);
+                }
+
+                @Override
+                public void noteRequestCompletion(final ServerStats stats,
+                        final Object response, final Throwable e,
+                        final long responseTime,
+                        final RetryHandler errorHandler) {
+                    System.out.println(
+                            "RibbonLoggingConfiguration.noteRequestCompletion");
+                    super.noteRequestCompletion(stats, response, e,
+                            responseTime, errorHandler);
+                }
+            };
+        }
+
         @Bean
         @ConditionalOnMissingBean
         public Class<? extends ILoadBalancer> loggingLoadBalancer() {
