c608f7d4ad5e5a8500d22d96d1c4c2b85455a644
Brian Oxley <boxley@thoughtworks.com>
2015-06-13 10:09:26 -0500

    Better name.

--- remote/src/test/java/hello/RemoteHelloSwaggerApiIT.java
+++ remote/src/test/java/hello/RemoteHelloSwaggerApiIT.java
@@ -1,7 +1,5 @@
 package hello;
 
-import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +12,7 @@ import static io.github.robwin.swagger.test.SwaggerAssertions.assertThat;
 import static java.lang.String.format;
 
 /**
- * {@code RemoteHelloApiIT} validates the implementation against the API.
+ * {@code RemoteHelloSwaggerApiIT} validates the implementation against the API.
  *
  * @author <a href="mailto:boxley@thoughtworks.com">Brian Oxley</a>
  */
@@ -22,22 +20,12 @@ import static java.lang.String.format;
 @SpringApplicationConfiguration(classes = RemoteHelloMain.class)
 @IntegrationTest("server.port=0")
 @WebAppConfiguration
-public class RemoteHelloApiIT {
+public class RemoteHelloSwaggerApiIT {
     @Value("${local.server.port}")
     private int port;
 
     @Test
     public void shouldMatchApi() {
-        final Swagger json = new SwaggerParser()
-                .read(format("http://localhost:%d/remote-hello/v2/api-docs",
-                        port));
-        final Swagger yaml = new SwaggerParser()
-                .read(getClass().getResource("/remote-hello-swagger2.yml")
-                        .getPath());
-
-        System.out.println("json = " + json);
-        System.out.println("yaml = " + yaml);
-
         assertThat(api()).isEqualTo(
                 getClass().getResource("/remote-hello-swagger2.yml")
                         .getPath());
