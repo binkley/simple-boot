50269c6acb00652ccf53a310334f21ddabc560b2
Brian Oxley <boxley@thoughtworks.com>
2015-12-10 21:02:17 -0500

    Multiple d/b's.

--- local/src/main/java/hello/HelloWorldMain.java
+++ local/src/main/java/hello/HelloWorldMain.java
@@ -6,9 +6,11 @@ import org.springframework.boot.actuate.health.CompositeHealthIndicator;
 import org.springframework.boot.actuate.health.HealthAggregator;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadata;
 import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.boot.autoconfigure.jdbc.metadata.DataSourcePoolMetadataProviders;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -28,6 +30,25 @@ import java.util.Map;
 public class HelloWorldMain {
     public static void main(final String... args) {
         SpringApplication.run(HelloWorldMain.class, args);
+    }
+
+    @Bean(name = "bob-dataSource")
+    @Primary
+    @ConfigurationProperties(prefix="datasource.bob")
+    public DataSource bobDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "nancy-dataSource")
+    @ConfigurationProperties(prefix="datasource.nancy")
+    public DataSource nancyDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "wick-fowler-dataSource")
+    @ConfigurationProperties(prefix="datasource.wick-fowler")
+    public DataSource wickFowlerDataSource() {
+        return DataSourceBuilder.create().build();
     }
 
     /** @todo Why does auto-config misses config class? */
